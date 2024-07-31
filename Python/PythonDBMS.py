21f4f49dfc08a9d9b98b41936e1ecfde8b3a1569
Haribalaji S <138091305+hari782@users.noreply.github.com>
2024-07-31 06:06:49 +0530

    Delete Python/PythonDBMS.py

--- Python/PythonDBMS.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-db = mysql.connector.connect(host = "localhost", db = "py", user = "root", password = "hari@2003")
-c = db.cursor()
-c.execute("select * from employees")
-res = c.fetchall()
-for res1 in res:
-    print(res1)
-db.commit()
